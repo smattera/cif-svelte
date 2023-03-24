6c5b054a83f8a2c1965954d086868015ebe3cde4
Sonny Mattera <smattera@hotmail.com>
2023-03-24 11:42:11 -0500

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
-<h1 class="text-3xl font-bold underline">
-	Hello world!
-</h1>
+<script>
+	import CIFVideoWeb from "$lib/images/CIFVideoWeb.mp4"
+</script>
 
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<svelte:head>
+	<title>Home | Choctaw Indian Fair</title>
+</svelte:head>
+
+<video id="bg-video" autoplay loop muted>
+	<source src={CIFVideoWeb} />
+</video>
+<div class="justify-center items-center">
+	<div class="bg-white shadow-2xl p-6">
+		<h1 class="text-5xl font-bold text-gray-800">Hello, world!</h1>
+		<p class="text-gray-600 mt-2">This is a background with daisyUI.</p>
+	</div>
+</div>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,34 @@
+<!-- <script>
+  import { NavLink } from "$lib/navLink";
+  const pages = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" },
+  ];
+</script>
+
+<nav>
+  <ul>
+    {#each pages as page}
+    <li>
+      <NavLink to={page.href}>{page.name}</NavLink>
+    </li>
+  </ul>
+</nav> -->
+
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <label for="my-drawer" class="btn btn-square btn-ghost">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+    </label>
+  </div>
+  <div class="navbar-center">
+    <a href="/" class="btn btn-ghost normal-case text-xl">Choctaw Indian Fair</a>
+  </div>
+  <div class="navbar-end">
+    <a href="/" class="btn btn-warning">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-auto" fill="currentColor" viewBox="0 96 960 960"><path d="M140 896q-26 0-43-17t-17-43V683q37-8 61.5-37.5T166 576q0-40-24.5-70T80 469V316q0-26 17-43t43-17h680q26 0 43 17t17 43v153q-37 7-61.5 37T794 576q0 40 24.5 69.5T880 683v153q0 26-17 43t-43 17H140Zm0-60h680V727q-38-26-62-65t-24-86q0-47 24-86t62-65V316H140v109q39 26 62.5 65t23.5 86q0 47-23.5 86T140 727v109Zm340-63q12 0 21-9t9-21q0-12-9-21t-21-9q-12 0-21 9t-9 21q0 12 9 21t21 9Zm0-167q12 0 21-9t9-21q0-12-9-21t-21-9q-12 0-21 9t-9 21q0 12 9 21t21 9Zm0-167q12 0 21-9t9-21q0-12-9-21t-21-9q-12 0-21 9t-9 21q0 12 9 21t21 9Zm0 137Z"/></svg>
+      <span class="hidden md:block ml-2">Buy Tickets</span>
+    </a>
+  </div>
+</div>
