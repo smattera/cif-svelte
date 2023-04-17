1e84857737ef046f4a7befd390a8c1c138d325e8
Sonny Mattera <smattera@hotmail.com>
2023-04-17 15:22:19 -0500

    Update events

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -20,12 +20,11 @@
   ];
 </script>
 
-<div class="hero bg-section py-10">
-	<div class="container mx-auto flex flex-col items-center h-full">
+<div class="hero min-h-screen bg-section">
+	<div class="container mx-auto flex items-center h-full">
 		<img src="/img/event-banner.webp" alt="Events" class="hidden md:block w-full h-auto">
 		<img src="/img/event-banner02.webp" alt="Events" class="md:hidden w-full h-auto mt-8 px-4">
 		<!--<h1 class="font-bold text-9xl" style="text-shadow:5px 5px 10px #333">EVENTS</h1>-->
-		<h1 class="font-bold text-5xl py-10 text-center" style="text-shadow:5px 5px 10px #333">2023 Schedule Coming Soon!</h1>
 
 		<!-- <div class="tabs tabs-boxed items-center align-middle gap-5 w-full">
       {#each dates as date}
