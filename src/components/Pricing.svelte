8da17bc06f8bb766f465a2e8ed9c2408ff3941f5
Sonny Mattera <smattera@hotmail.com>
2023-04-17 08:21:35 -0500

    Update homepage

--- src/components/Pricing.svelte
+++ src/components/Pricing.svelte
@@ -1,30 +1,42 @@
-<div class="hero pt-8 bg-secondary">
-  <h1 class="text-3xl font-bold text-center" style="text-shadow:5px 5px 10px #333">Ticket Presale Starts Soon</h1>
-</div>
-
-<div class="hero pb-4 bg-secondary text-center">
-	<div class="grid grid-cols-1 md:grid-cols-3 gap-0 md:gap-12 m-4 mb-8">
-		<!-- Pricing Tier One-->
-		<div class="bg-green-600 rounded-lg shadow-lg mb-4 md:mb-0">
-			<div class="p-8 glass rounded-lg outline outline-warning outline-offset-4 w-full h-full">
-				<h3 class="text-5xl font-bold">Child Pass</h3>
-				<p class="my-4 text-gray-600">Perfect for small businesses</p>
-				<p class="text-5xl text-warning font-bold">FREE</p>
+<div class="bg-pricing">
+	<div class="hero pt-8">
+		<h1 class="text-3xl font-bold text-center">Ticket Presale Starts Soon</h1>
+	</div>
+	<div class="hero pb-4 text-center">
+		<div class="grid grid-cols-1 md:grid-cols-3 gap-0 md:gap-12 m-4 mb-8">
+			<!-- Pricing Tier One-->
+			<div class="bg-green-600 rounded-lg shadow-lg mb-4 md:mb-0">
+				<div class="p-8 glass rounded-lg outline outline-neutral outline-offset-4 w-full h-full">
+					<h3 class="text-5xl font-bold">Child Pass</h3>
+					<p class="my-4 text-gray-600 text-2xl">Ages 5 & Under</p>
+					<p class="text-5xl text-warning font-bold">FREE</p>
+				</div>
 			</div>
-		</div>
-    <div class="bg-gradient-to-b from-blue-700 via-red-500 to-yellow-300 rounded-lg shadow-lg mb-4 md:mb-0">
-			<div class="p-8 glass rounded-lg outline outline-warning outline-offset-4 w-full h-full">
-				<h3 class="text-5xl font-bold">Student Pass</h3>
-				<p class="my-4 text-gray-600">Perfect for small businesses</p>
-				<p class="text-5xl text-warning font-bold">$29</p>
+			<div class="bg-gradient-to-b from-blue-700 via-red-500 to-yellow-300 rounded-lg shadow-lg mb-4 md:mb-0">
+				<div class="p-8 glass rounded-lg outline outline-neutral outline-offset-4 w-full h-full">
+					<h3 class="text-5xl font-bold">Student Pass</h3>
+					<p class="my-4 text-gray-600 text-2xl">Ages 6 - 17</p>
+					<p class="text-5xl font-bold">Season $10</p>
+					<p class="text-5xl font-bold">Day $7</p>
+				</div>
 			</div>
-		</div>
-    <div class="bg-blue-400 rounded-lg shadow-lg">
-			<div class="p-8 glass rounded-lg outline outline-warning outline-offset-4 h-full w-full">
-				<h3 class="text-5xl font-bold">Adult Pass</h3>
-				<p class="my-4 text-gray-600">Perfect for small businesses</p>
-				<p class="text-5xl text-warning font-bold">$29</p>
+			<div class="bg-blue-400 rounded-lg shadow-lg">
+				<div class="p-8 glass rounded-lg outline outline-neutral outline-offset-4 h-full w-full">
+					<h3 class="text-5xl font-bold">Adult Pass</h3>
+					<p class="my-4 text-gray-600 text-2xl">Ages 18 & Over</p>
+					<p class="text-5xl text-warning font-bold">Season $20</p>
+					<p class="text-5xl text-warning font-bold">Day $12</p>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
+<style>
+	.bg-pricing {
+    background: url("/lattice.svg") center repeat,
+    #212527;
+		background-size: auto;
+		background-blend-mode: lighten;
+	}
+</style>
